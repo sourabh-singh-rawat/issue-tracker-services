cc3d3b6e079c2e4eff4580b2f3b0795627ffbf4c
Sourabh Singh Rawat <Sourabh.rawatcc@gmail.com>
2022-04-23 23:41:28 +0530

    added route for projects, added data grid component for displaying project in table

--- frontend/src/components/create-project/create-project.component.jsx
+++ frontend/src/components/create-project/create-project.component.jsx
@@ -1,5 +1,14 @@
 import { useState } from "react";
-import { Grid, Box, Button, Modal, TextField, Typography } from "@mui/material";
+import { connect } from "react-redux";
+import {
+  Grid,
+  Box,
+  Button,
+  Modal,
+  TextField,
+  Typography,
+  LinearProgress,
+} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
 const style = {
@@ -7,90 +16,147 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: "50%",
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
-const CreateProject = () => {
+const CreateProject = (props) => {
+  const { uid, email = "No owner" } = props;
   const [open, setOpen] = useState(false);
+  const [projectFields, setProjectFields] = useState({
+    name: "",
+    description: "",
+    uid,
+    email,
+    startDate: null,
+    endDate: null,
+  });
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(projectFields);
+    // TODO: send to backend
+    await fetch("http://localhost:4000/api/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(projectFields),
+    });
+  };
+
+  const handleChange = (e) => {
+    const { name } = e.target;
+    setProjectFields({ ...projectFields, [name]: e.target.value });
+  };
+
   return (
-    <Box component="div">
+    <Box>
       <Button color="primary" startIcon={<AddIcon />} onClick={handleOpen}>
         Create
       </Button>
-
       <Modal open={open} onClose={handleClose}>
-        <Box sx={style}>
-          <Grid container columnSpacing={2} rowSpacing={1}>
-            <Grid item sm={12} paddingBottom="1em">
-              <Typography variant="h4">Create Project</Typography>
-            </Grid>
-            <Grid item sm={12}>
-              <TextField
-                label="Project Name"
-                variant="outlined"
-                margin="dense"
-                required
-                fullWidth
-              />
-            </Grid>
-            <Grid item sm={12}>
-              <TextField
-                label="Owner"
-                variant="outlined"
-                margin="normal"
-                value={"John Doe"}
-                autoFocus={true}
-                fullWidth
-                disabled
-              />
-            </Grid>
-            <Grid item sm={12}></Grid>
+        <Box>
+          <LinearProgress color="secondary" sx={{ display: "none" }} />
+          <Box sx={style} component="form" onSubmit={handleSubmit}>
+            <Grid container columnSpacing={2} rowSpacing={1}>
+              <Grid item sm={12} paddingBottom="1em">
+                <Typography variant="h4">Create Project</Typography>
+              </Grid>
+              <Grid item sm={12}>
+                <TextField
+                  name="name"
+                  label="Project Name"
+                  variant="outlined"
+                  margin="dense"
+                  onChange={handleChange}
+                  required
+                  fullWidth
+                />
+              </Grid>
+              <Grid item sm={12}>
+                <TextField
+                  label="description"
+                  name="description"
+                  rows={4}
+                  margin="dense"
+                  onChange={handleChange}
+                  multiline
+                  fullWidth
+                />
+              </Grid>
+              <Grid item sm={12}>
+                <TextField
+                  name="email"
+                  label="Owner"
+                  variant="outlined"
+                  margin="normal"
+                  value={email}
+                  onChange={handleChange}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  autoFocus={true}
+                  fullWidth
+                  disabled
+                />
+              </Grid>
+              <Grid item sm={12}></Grid>
 
-            <Grid item sm={6}>
-              <TextField
-                label="Start Date"
-                type="date"
-                margin="dense"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                fullWidth
-              />
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="startDate"
+                  label="Start Date"
+                  type="date"
+                  margin="dense"
+                  onChange={handleChange}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} paddingBottom="1em">
+                <TextField
+                  name="endDate"
+                  label="End Date"
+                  type="date"
+                  margin="dense"
+                  onChange={handleChange}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" type="submit" fullWidth>
+                  Create Project
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button variant="outlined" onClick={handleClose} fullWidth>
+                  Cancel
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item sm={6} paddingBottom="1em">
-              <TextField
-                label="End Date"
-                type="date"
-                margin="dense"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                fullWidth
-              />
-            </Grid>
-            <Grid item sm={6}>
-              <Button variant="contained" fullWidth>
-                Create Project
-              </Button>
-            </Grid>
-            <Grid item sm={6}>
-              <Button variant="outlined" onClick={handleClose} fullWidth>
-                Cancel
-              </Button>
-            </Grid>
-          </Grid>
+          </Box>
         </Box>
       </Modal>
     </Box>
   );
 };
 
-export default CreateProject;
+const mapStateToProps = (state) => {
+  return {
+    uid: state.user.uid,
+    email: state.user.email,
+  };
+};
+export default connect(mapStateToProps)(CreateProject);
--- frontend/src/routes/projects/projects.component.jsx
+++ frontend/src/routes/projects/projects.component.jsx
@@ -1,17 +1,73 @@
+import { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+
 // MUI Styles
 import { Box, Grid, Toolbar, Typography } from "@mui/material";
 import AppBarContainer from "../../components/appbar-container/appbar-container.component";
 import CreateProject from "../../components/create-project/create-project.component";
 
 const Projects = (props) => {
+  const [rows, setRows] = useState([]);
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      width: 90,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      width: 250,
+    },
+    {
+      field: "owner_email",
+      headerName: "Owner",
+      width: 250,
+    },
+    {
+      field: "start_date",
+      headerName: "Start Date",
+      width: 200,
+    },
+    {
+      field: "end_date",
+      headerName: "End Date",
+      width: 200,
+    },
+  ];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("http://localhost:4000/api/projects", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await result.json();
+      setRows(data);
+    };
+    fetchData();
+  });
+
   return (
     <Box>
       <AppBarContainer element={<CreateProject />}>Projects</AppBarContainer>
       <Toolbar sx={{ borderBottom: "3px solid #f4f4f4" }}>
         <Typography>All Projects</Typography>
       </Toolbar>
-      <Grid container>
-        <Grid item></Grid>
+      <Grid container sx={{ height: "80vh" }}>
+        <Grid item xs={12}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={10}
+            rowsPerPageOptions={[10]}
+            sx={{ border: "none" }}
+            disableSelectionOnClick
+          ></DataGrid>
+        </Grid>
       </Grid>
     </Box>
   );
