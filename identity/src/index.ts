30dcc0146ab59e70aca05600c7bebd7639931a73
Sourabh Singh Rawat <sourabh.rawatcc@gmail.com>
2023-08-12 07:19:51 +0530

    chore: updated imports for core-utils, removed .npmrc, and other changes accordingly

--- identity/src/index.ts
+++ identity/src/index.ts
@@ -1,5 +1,4 @@
 import { app } from "./app";
-import { pgContext } from "./config/pg-context.config";
 
 const PORT = process.env.PORT || 4000;
 
@@ -7,8 +6,6 @@ const main = async () => {
   app.listen(PORT, () => {
     console.log(`Identity service is up and running on port ${PORT}`);
   });
-
-  console.log((await pgContext.query("SELECT NOW()", [])).rows);
 };
 
 main();
