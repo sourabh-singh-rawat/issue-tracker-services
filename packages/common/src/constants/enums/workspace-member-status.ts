7f5cfca325de015c046ec0f5f15bd06fc7917e4b
sourabh-singh-rawat <sourabh.rawatcc@gmail.com>
2024-06-30 15:48:20 +0530

    fix: Update workspace member status constant

--- packages/common/src/constants/enums/workspace-member-status.ts
+++ packages/common/src/constants/enums/workspace-member-status.ts
@@ -1,5 +1,12 @@
-export enum WorkspaceMemberStatus {
-  PENDING = "Pending",
-  INVITED = "Invited",
-  ACTIVE = "Active",
-}
+export const WORKSPACE_MEMBER_STATUS = {
+  ACTIVE: "Active",
+  PENDING: "Pending",
+  INVITED: "Invited",
+  REMOVED: "Removed",
+  SUSPENED: "Suspended",
+  BLOCKED: "Blocked",
+  DELETED: "Deleted",
+} as const;
+
+export type WorkspaceMemberStatus =
+  (typeof WORKSPACE_MEMBER_STATUS)[keyof typeof WORKSPACE_MEMBER_STATUS];
