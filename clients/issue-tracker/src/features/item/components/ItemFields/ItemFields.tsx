f5abc7a0b9519b094376845615714c3ca618eb25
sourabh-singh-rawat <sourabh.rawatcc@gmail.com>
2025-01-08 12:13:26 +0530

    Fix: Update item system fields

--- clients/issue-tracker/src/features/item/components/ItemFields/ItemFields.tsx
+++ clients/issue-tracker/src/features/item/components/ItemFields/ItemFields.tsx
@@ -1,4 +1,9 @@
-import { Grid2, Typography } from "@mui/material";
+import {
+  CalendarMonthOutlined,
+  CircleOutlined,
+  FlagOutlined,
+} from "@mui/icons-material";
+import { Grid2, Stack, Typography, useTheme } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { UpdateItemMutationOptions } from "../../../../api";
 import { ItemPrioritySelector } from "../ItemPrioritySelector";
@@ -12,66 +17,77 @@ interface ItemFieldsProps {
   updateItem: (options: UpdateItemMutationOptions) => Promise<any>;
 }
 
-export default function ItemFields({
+export const ItemFields = ({
   itemId,
   listId,
   statusId,
   priority,
   updateItem,
-}: ItemFieldsProps) {
+}: ItemFieldsProps) => {
+  const theme = useTheme();
   const statusForm = useForm({ defaultValues: { statusId } });
   const priorityForm = useForm({ defaultValues: { priority } });
 
+  const rows = [
+    {
+      icon: <CircleOutlined fontSize="small" />,
+      name: "Status",
+      component: (
+        <ItemStatusSelector
+          form={statusForm}
+          listId={listId}
+          name="statusId"
+          onSubmit={async (value) => {
+            updateItem({ variables: { input: { itemId, statusId: value } } });
+          }}
+        />
+      ),
+    },
+    {
+      icon: <FlagOutlined fontSize="small" />,
+      name: "Priority",
+      component: (
+        <ItemPrioritySelector
+          form={priorityForm}
+          name="priority"
+          onSubmit={async (value) => {
+            updateItem({ variables: { input: { itemId, priority: value } } });
+          }}
+          options={["Urgent", "High", "Normal", "Low"]}
+        />
+      ),
+    },
+    {
+      icon: <CalendarMonthOutlined fontSize="small" />,
+      name: "Dates",
+      component: <Typography>Empty</Typography>,
+    },
+  ];
+
   return (
-    <Grid2 container rowSpacing={2}>
-      <Grid2 size={12}>
-        <Grid2 container>
-          <Grid2 size={3}>
-            <Typography variant="body1">Status</Typography>
+    <Grid2 container columnSpacing={4} rowSpacing={2}>
+      {rows.map(({ icon, name, component }, index) => {
+        return (
+          <Grid2 size={{ xs: 12, md: 6 }} key={index}>
+            <Grid2 container>
+              <Grid2
+                flexGrow={1}
+                sx={{
+                  alignItems: "center",
+                  alignContent: "center",
+                  color: theme.palette.text.secondary,
+                }}
+              >
+                <Stack direction="row" spacing={theme.spacing(0.5)}>
+                  <div>{icon}</div>
+                  <Typography variant="body1">{name}</Typography>
+                </Stack>
+              </Grid2>
+              <Grid2 size={8}>{component}</Grid2>
+            </Grid2>
           </Grid2>
-          <Grid2 size={3}>
-            <ItemStatusSelector
-              form={statusForm}
-              listId={listId}
-              name="statusId"
-              onSubmit={async (value) => {
-                updateItem({
-                  variables: { input: { itemId, statusId: value } },
-                });
-              }}
-            />
-          </Grid2>
-        </Grid2>
-      </Grid2>
-
-      <Grid2 size={12}>
-        <Grid2 container>
-          <Grid2 size={3}>
-            <Typography variant="body1">Priority</Typography>
-          </Grid2>
-          <Grid2 size={3}>
-            <ItemPrioritySelector
-              form={priorityForm}
-              name="priority"
-              onSubmit={async (value) => {
-                updateItem({
-                  variables: { input: { itemId, priority: value } },
-                });
-              }}
-              options={["Urgent", "High", "Normal", "Low"]}
-            />
-          </Grid2>
-        </Grid2>
-      </Grid2>
-
-      <Grid2 size={12}>
-        <Grid2 container>
-          <Grid2 size={3}>
-            <Typography variant="body1">Dates</Typography>
-          </Grid2>
-          <Grid2 size={3}>Empty</Grid2>
-        </Grid2>
-      </Grid2>
+        );
+      })}
     </Grid2>
   );
-}
+};
